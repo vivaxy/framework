e1bfd99801ab0ccc5151654900b52c176e7f3708
vivaxy <xyxuye2007@126.com>
2018-12-06 18:40:02 +0800

    feat: :sparkles:

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # event-based-framework
+
 Event Based Framework
+
+# Usage
+
+## In a new project
+
+`git submodule add git@github.com:vivaxy/event-based-framework.git`
+
+## In a initialized project
+
+`git submodule init && git submodule update`
---
+++ class/event-emitter.js
@@ -0,0 +1,83 @@
+/**
+ * @since 2018-06-14 14:05:19
+ * @author vivaxy
+ */
+
+export default class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  /**
+   *
+   * @param event
+   * @param callback
+   * @param refer
+   * @returns {EventEmitter}
+   */
+  on(event, callback, refer) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    // console.log('EventEmitter.on', event, callback, refer);
+    this.events[event].push({ callback, refer });
+    return this;
+  }
+
+  /**
+   * Do not using polymorphism
+   * @param event
+   * @param data
+   * @param sender
+   * @returns {EventEmitter}
+   */
+  emit(event, data, sender) {
+    // console.group('EventEmitter.emit');
+    // console.log('%cEventEmitter.emit.event', 'background-color: #aaaaff;', event);
+    // console.log('%cEventEmitter.emit.data', 'background-color: #aaaaff;', data);
+    // console.log('%cEventEmitter.emit.sender', 'background-color: #aaaaff;', sender);
+    const handlers = this.events[event];
+    if (handlers) {
+      for (let i = 0; i < handlers.length;) {
+        const handler = handlers[i];
+        if (handler) {
+          handler.callback(event, data, sender, handler.refer);
+          // console.log('%cEventEmitter.emit.callback', 'background-color: #aaaaff;', handler.callback);
+          if (handler === handlers[i]) {
+            i++;
+          }
+        }
+      }
+    }
+    // console.groupEnd();
+    return this;
+  }
+
+  /**
+   *
+   * @param event
+   * @param callback
+   * @param refer
+   * @returns {EventEmitter}
+   */
+  off(event, callback, refer) {
+    const handlers = this.events[event];
+    if (handlers) {
+      const handlersLength = handlers.length;
+      for (let i = 0; i < handlersLength; i++) {
+        const handler = handlers[i];
+        if (handler.callback === callback && handler.refer === refer) {
+          handlers.splice(i, 1);
+          if (handlers.length === 0) {
+            delete this.events[event];
+          }
+          return this;
+        }
+      }
+    } else {
+      this.events[event] = [];
+    }
+    return this;
+  }
+
+}
