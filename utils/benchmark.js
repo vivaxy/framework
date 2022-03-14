f50e5b176a31a181f08fdcd6ff06b7b1dbd5d517
vivaxy <xyxuye2007@126.com>
2022-03-14 13:21:12 +0800

    feat(benchmark):  add `benchmark`

---
+++ utils/benchmark.js
@@ -0,0 +1,41 @@
+/**
+ * @since 2022-03-14
+ * @author vivaxy
+ */
+function NOOP() {}
+
+function now() {
+  return (performance || Date).now();
+}
+
+export async function run(
+  fn,
+  {
+    beforeAll = NOOP,
+    afterAll = NOOP,
+    beforeEach = NOOP,
+    afterEach = NOOP,
+    loop = 100,
+  },
+) {
+  const isAsync = fn.toString().startsWith('async');
+  const ctx = {};
+  await beforeAll(ctx);
+  let cost = 0;
+  for (let i = 0; i < loop; i++) {
+    ctx.loopIndex = i;
+    await beforeEach(ctx);
+
+    const startTime = now();
+    if (isAsync) {
+      await fn(ctx);
+    } else {
+      fn(ctx);
+    }
+    cost += now() - startTime;
+
+    await afterEach(ctx);
+  }
+  await afterAll(ctx);
+  return cost / loop;
+}
